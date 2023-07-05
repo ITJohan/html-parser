0a9d67c8035712dd3660562d1c601cadd30c3899
itjohan <johanlindkvist89@gmail.com>
2023-07-05 16:43:50 +0200

    feat: flexible html parser

--- TestComponent.js
+++ TestComponent.js
@@ -4,11 +4,27 @@ import { html } from './html-parser.js';
 
 class TestComponent extends HTMLElement {
   shadowRoot = this.attachShadow({ mode: 'open' });
+  text = 'Hello world!';
   label = 'Click me';
 
   constructor() {
     super();
-    this.shadowRoot.appendChild(html`<button>${this.label}</button>`);
+    this.shadowRoot.appendChild(
+      html`
+        <main>
+          ${this.text}
+          <section>
+            <p>Hello</p>
+            <button @click=${() => console.log('testing')}>
+              ${this.label}
+            </button>
+          </section>
+          <footer>This is the footer</footer>
+          <p>${this.text}</p>
+          ${this.label}
+        </main>
+      `
+    );
   }
 }
 
--- html-parser.js
+++ html-parser.js
@@ -1,28 +1,115 @@
 // @ts-check
 
+/** @type {Map<string, string>} */
+const textMap = new Map();
+/** @type {Map<string, function>} */
+const callbackMap = new Map();
+
+let id = 0;
+
 /** @type {(strings: TemplateStringsArray, ...values: (string | function)[]) => Node} */
-export function html(strings, ...values) {
-  const template = document.createElement('template');
+export const html = (strings, ...variables) => {
+  const placeholderString = strings.reduce(
+    (placeholderString, string, index) => {
+      const variable = variables[index];
 
-  console.log(strings);
-  console.log(values);
+      if (typeof variable === 'string') {
+        const placeholderId = `text-${id}`;
+        const partialPlaceholderString = string + placeholderId;
+        textMap.set(placeholderId, variable);
+        id++;
 
-  strings.map((string, index) => {
-    const regex = /<(\w+)/;
-    const match = string.match(regex);
-
-    if (match) {
-      const isolatedElementType = match[1];
-      const element = document.createElement(isolatedElementType);
-
-      if (values[index]) {
-        const string = String(values[index]);
-        element.appendChild(document.createTextNode(string));
+        return placeholderString + partialPlaceholderString;
       }
 
-      template.content.appendChild(element);
+      if (typeof variable === 'function') {
+        const eventType = string.split('@')[1].split('=')[0];
+        const placeholderId = `${eventType}-${id}`;
+        const partialPlaceholderString = string.replace(
+          `@${eventType}=`,
+          placeholderId
+        );
+        callbackMap.set(placeholderId, variable);
+        id++;
+
+        return placeholderString + partialPlaceholderString;
+      }
+
+      return placeholderString + string;
+    },
+    ''
+  );
+
+  const parser = new DOMParser();
+  const dom = parser.parseFromString(placeholderString, 'text/html');
+  const template = document.createElement('template');
+
+  Array.from(dom.body.children).forEach((child) => {
+    template.content.appendChild(child);
+  });
+
+  const templateClone = template.content.cloneNode(true);
+  replacePlaceholders(templateClone, textMap, callbackMap);
+
+  return templateClone;
+};
+
+/** @type {(node: Node, textMap: Map<string, string>, callbackMap: Map<string, function>) => void} */
+const replacePlaceholders = (node, textMap, callbackMap) => {
+  textMap.forEach((text, placeholderId) => {
+    const textNode = findTextNode(node, placeholderId);
+
+    if (textNode) {
+      textNode.textContent = text;
     }
   });
 
-  return template.content.cloneNode(true);
-}
+  callbackMap.forEach((callback, placeholderId) => {
+    const [eventType] = placeholderId.split('-');
+    const callbackNode = findCallbackNode(node, placeholderId);
+
+    if (callbackNode) {
+      // @ts-ignore
+      callbackNode.addEventListener(eventType, callback);
+      callbackNode.removeAttribute(placeholderId);
+    }
+  });
+};
+
+/** @type {(node: Node, placeholderId: string) => Node | undefined} */
+const findTextNode = (node, placeholderId) => {
+  if (node.textContent?.trim() === placeholderId) {
+    return node;
+  }
+
+  if (node.hasChildNodes()) {
+    for (const childNode of node.childNodes) {
+      const result = findTextNode(childNode, placeholderId);
+
+      if (result) {
+        return result;
+      }
+    }
+  }
+
+  return undefined;
+};
+
+/** @type {(node: Node, placeholderId: string) => HTMLElement | undefined} */
+const findCallbackNode = (node, placeholderId) => {
+  if (node instanceof HTMLElement && node.hasAttribute(placeholderId)) {
+    return node;
+  }
+
+  if (node.hasChildNodes()) {
+    for (const childNode of node.childNodes) {
+      const result = findCallbackNode(childNode, placeholderId);
+
+      if (result) {
+        return result;
+      }
+    }
+  }
+
+  return undefined;
+};
